6477539586aa5328a388f07f81422e0afdcc8eac
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-08 16:53:40 -0400

    Completed index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+
+const sum = function(fromN, toN) {
+// Sum all the values from fromN up to toN
+//   So if we called the function with the following values: sum(3, 7), it would sum 3 + 4 + 5 + 6 + 7 and output 25.
+// For this exercise, we can safely assume that fromN will always be less than or equal to toN.
+  let result = 0;
+
+  if (fromN <= toN) {
+    return fromN + sum(fromN + 1, toN);
+  }
+  return result;
+};
 
 module.exports = sum;
+//console.log(sum(3,7));
